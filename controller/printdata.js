a48828252dc855adcdbcfdf9bead7ddde7e9d4c2
ruchitakb7 <ruchitakb7@gmail.com>
2024-01-21 17:17:44 +0530

    appintmntapp

---
+++ controller/printdata.js
@@ -0,0 +1,111 @@
+const path= require('path');
+
+const express=require('express')
+const router=express.Router();
+
+//const data=require('../data/array');
+
+const modelclass= require('../model/model.js');
+
+exports.datacontroller = (req,res,next)=>
+{
+  
+    res.render('forms',{
+        path:'/data'
+    })
+}
+
+exports.savedatacontoller= (req,res,next) =>
+{
+  
+  const name = req.body.name;
+   const city= req.body.city;
+   const email = req.body.email;
+   console.log(email)
+
+   modelclass.create({
+    name:name,
+    city:city,
+    email:email,
+   })
+   .then(()=>{
+    res.redirect('/print') ;
+   })
+   .catch((err)=>{
+    res.send('Not Found')
+   })
+
+}
+
+exports.fetch= (req,res,next)  =>{
+modelclass.findAll()
+.then((result)=>{
+    res.render('table',{
+        prod:result
+    })
+ 
+}).catch((err)=>{
+    console.log(err)
+})
+}
+
+exports.show=(req,res,next) =>{
+    let userid=req.params['id']
+
+   modelclass.findAll({where:{id:userid}})
+    .then((result)=>{
+        res.render('product_details',{
+        prod:result
+        });
+       
+    }).catch((err)=>{
+        res.send("Not Found")
+    })
+}
+
+exports.delete= (req,res,next)=>{
+    
+    let userid=req.params['id']
+    console.log(userid)
+
+    modelclass.destroy({where:{id:userid}})
+    .then((result)=>{
+        res.redirect('/print')
+    })
+    .catch((e)=>{
+        res.send("No Found")
+    })
+    
+}
+
+exports.update = (req,res,next) =>{
+
+    let userid = req.params['id']
+
+   modelclass.findAll({where:{id:userid}})
+    .then((result)=>{
+      
+        res.render('update',{
+        prod:result
+        });
+       
+    }).catch((err)=>{
+        res.send("Not Found")
+    })
+}
+
+exports.edit= async (req,res,next) =>{
+    const p={
+        name :req.body.name,
+        city: req.body.city,
+        email: req.body.email
+    }
+     const userid= req.body.id  
+   await modelclass.update(p,{where:{id:userid}})
+   .then((result)=>{
+    console.log(result);
+       res.redirect('/print')
+   })
+   .catch((e)=>{console.log(e)})
+}
+
---
+++ routes/forms.js
@@ -0,0 +1,31 @@
+const express=require('express');
+
+const router= express.Router();
+
+const path= require('path');
+
+const improtingfromcontroller= require('../controller/printdata');
+  /* router.get('/data',(req,res)=>{
+ arr.forEach(element => {
+        console.log(element.name)
+    });
+    res.send("data has been printed"); 
+}) */
+
+router.get('/form',improtingfromcontroller.datacontroller )
+
+router.post('/add',improtingfromcontroller.savedatacontoller);
+
+
+router.use('/print', improtingfromcontroller.fetch);
+
+router.use('/product/:id',improtingfromcontroller.show);
+
+router.use('/delete/:id',improtingfromcontroller.delete);
+
+
+
+router.use('/update/:id', improtingfromcontroller.update);
+
+router.post('/edit',improtingfromcontroller.edit);
+module.exports=router;
